8df67afabda47381d460f77a51c9ddc20c3d6032
nirbhaypatil <nirbhaypatil@gmail.com>
2023-08-02 16:33:57 +0530

    changed comment.

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,1 +1,38 @@
-package TestRunner;import ThreadLocalUsageSelenium.DriverFactory;import io.cucumber.testng.AbstractTestNGCucumberTests;import io.cucumber.testng.CucumberOptions;import org.openqa.selenium.WebDriver;import org.testng.annotations.AfterMethod;import org.testng.annotations.BeforeMethod;import org.testng.annotations.DataProvider;@CucumberOptions(                    features ="src/test/java/resources/",                    glue = {"steps"},                    plugin = {"pretty","summary"},                    monochrome = true                )public class TestRunner extends AbstractTestNGCucumberTests {    DriverFactory driverFactory;    @BeforeMethod    public void setUp() {        driverFactory = new DriverFactory();        driverFactory.setDriver();        System.out.println("Inside before method");    }    @DataProvider(parallel = true)    @Override    public Object[][] scenarios() {        return super.scenarios();    }    @AfterMethod    public void tear(){        driverFactory.getWebDriver().quit();    }}
+package TestRunner;
+
+
+import ThreadLocalUsageSelenium.DriverFactory;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+                    features ="src/test/java/resources/",
+                    glue = {"steps"},
+                    plugin = {"pretty","summary"},
+                    monochrome = true
+                )
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    DriverFactory driverFactory;
+
+    @BeforeMethod
+    public void setUp() {
+        driverFactory = new DriverFactory();
+
+        driverFactory.setDriver();
+        System.out.println("Inside before method");
+    }
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+    @AfterMethod
+    public void tear(){
+        driverFactory.getWebDriver().quit();
+    }
+}
